63b4ceeb4b3a288752b1ded7b725450c1c2d4eeb
mleadbeater <mleadbeater@me.com>
2013-12-27 13:38:40 +1030

    implement clear list

---
+++ iod/ClearListAction.cpp
@@ -0,0 +1,72 @@
+/*
+  Copyright (C) 2012 Martin Leadbeater, Michael O'Connor
+
+  This file is part of Latproc
+
+  Latproc is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  Latproc is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Latproc; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "ClearListAction.h"
+#include "MachineInstance.h"
+#include "Logger.h"
+
+ClearListActionTemplate::ClearListActionTemplate(Value destination) : dest_name(destination.asString()) {
+}
+
+ClearListActionTemplate::~ClearListActionTemplate() {
+}
+                                           
+Action *ClearListActionTemplate::factory(MachineInstance *mi) {
+	return new ClearListAction(mi, this);
+}
+
+ClearListAction::ClearListAction(MachineInstance *m, const ClearListActionTemplate *dat)
+    : Action(m), dest(dat->dest_name), dest_machine(0) {
+}
+
+ClearListAction::ClearListAction() : dest_machine(0) {
+}
+
+std::ostream &ClearListAction::operator<<(std::ostream &out) const {
+	return out << "Clear List Action " << dest << "\n";
+}
+
+Action::Status ClearListAction::run() {
+	owner->start(this);
+    if (!dest_machine)
+        dest_machine = owner->lookup(dest);
+	if (dest_machine && dest_machine->_type == "LIST") {
+        dest_machine->parameters.clear();
+        dest_machine->setNeedsCheck();
+        status = Complete;
+	}
+    else
+        status = Failed;
+    owner->stop(this);
+	return status;
+}
+
+Action::Status ClearListAction::checkComplete() {
+	if (status == Complete || status == Failed) return status;
+	if (this != owner->executingCommand()) {
+		DBG_MSG << "checking complete on " << *this << " when it is not the top of stack \n";
+	}
+	else {
+		status = Complete;
+		owner->stop(this);
+	}
+	return status;
+}
+
---
+++ iod/ClearListAction.h
@@ -0,0 +1,59 @@
+/*
+  Copyright (C) 2012 Martin Leadbeater, Michael O'Connor
+
+  This file is part of Latproc
+
+  Latproc is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  Latproc is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Latproc; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef __ClearListAction_H__
+#define __ClearListAction_H__ 1
+
+#include <iostream>
+#include "Action.h"
+#include "symboltable.h"
+
+class MachineInstance;
+
+struct ClearListActionTemplate : public ActionTemplate {
+    
+    // the bitset property named in the source parameter are used to set the state of
+    // entries in the list named in the dest parameter
+    // The names are to be evaluated in the
+    // scope where the command is used.
+    
+	ClearListActionTemplate(Value destination);
+    ~ClearListActionTemplate();
+    
+    virtual Action *factory(MachineInstance *mi);
+    std::ostream &operator<<(std::ostream &out) const {
+       return out << "Clear List " << dest_name << "\n";
+    }
+
+    std::string dest_name;
+};
+
+struct ClearListAction : public Action {
+	ClearListAction(MachineInstance *m, const ClearListActionTemplate *dat);
+    ClearListAction();
+	Status run();
+	Status checkComplete();
+    virtual std::ostream &operator<<(std::ostream &out)const;
+
+    Value dest;
+    MachineInstance *dest_machine;
+};
+
+#endif
