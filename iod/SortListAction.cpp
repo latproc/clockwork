ca09cd2abf73ee79972731cea8a93a92cbbd3b4f
mleadbeater <mleadbeater@me.com>
2013-12-31 09:44:28 +1030

    support sorting lists of values (string / integer)

--- iod/SortListAction.cpp
+++ iod/SortListAction.cpp
@@ -47,6 +47,23 @@ std::ostream &SortListAction::operator<<(std::ostream &out) const {
 	return out << "Sort List Action " << list_machine->getName() << " BY PROPERTY " << property_name;
 }
 
+class StringValueSorter {
+public:
+    bool operator()(const Parameter &a, const Parameter &b) const {
+        return a.val.asString() < b.val.asString();
+    }
+};
+
+class NumericValueSorter {
+public:
+    bool operator()(const Parameter &a, const Parameter &b) const {
+        if (a.val.kind != Value::t_integer || b.val.kind != Value::t_integer) return true;
+        long a_i, b_i;
+        if (!a.val.asInteger(a_i) || !b.val.asInteger(b_i)) return true;
+        return a_i < b_i;
+    }
+};
+
 class PropertyValueStringSorter {
 public:
     PropertyValueStringSorter(std::string property_name) : property(property_name) { }
@@ -79,22 +96,49 @@ Action::Status SortListAction::run() {
     if (!list_machine)
         list_machine = owner->lookup(list_machine_name);
 	if (list_machine && list_machine->_type == "LIST") {
-        bool use_integer_sort = true;
-        long val;
-        for (unsigned int i=0; i<list_machine->parameters.size(); ++i) {
-            MachineInstance *entry = list_machine->parameters[i].machine;
-            if (entry && !entry->getValue(property_name.asString()).asInteger(val)) {
-                use_integer_sort = false;
-                break;
+        
+        if (list_machine->parameters.empty())
+            status = Complete;
+        else if (list_machine->parameters.at(0).val.kind == Value::t_symbol) {
+            
+            bool use_integer_sort = true;
+            
+            long val;
+            // if the given property of any item in the list is not an integer, use a string sort
+            for (unsigned int i=0; i<list_machine->parameters.size(); ++i) {
+                MachineInstance *entry = list_machine->parameters[i].machine;
+                if (entry && !entry->getValue(property_name.asString()).asInteger(val)) {
+                    use_integer_sort = false;
+                    break;
+                }
+            }
+            if (use_integer_sort) {
+                PropertyValueNumericSorter property_sort(property_name.asString());
+                std::sort(list_machine->parameters.begin(), list_machine->parameters.end(), property_sort);
+            }
+            else {
+                PropertyValueStringSorter property_sort(property_name.asString());
+                std::sort(list_machine->parameters.begin(), list_machine->parameters.end(), property_sort);
             }
         }
-        if (use_integer_sort) {
-            PropertyValueNumericSorter property_sort(property_name.asString());
-            std::sort(list_machine->parameters.begin(), list_machine->parameters.end(), property_sort);
-        }
         else {
-            PropertyValueStringSorter property_sort(property_name.asString());
-            std::sort(list_machine->parameters.begin(), list_machine->parameters.end(), property_sort);
+            bool use_integer_sort = true;
+            
+            // if any item in the list is not an integer, use a string sort
+            for (unsigned int i=0; i<list_machine->parameters.size(); ++i) {
+                if (list_machine->parameters.at(i).val.kind != Value::t_integer) {
+                    use_integer_sort = false;
+                    break;
+                }
+            }
+            if (use_integer_sort) {
+                NumericValueSorter numeric_sort;
+                std::sort(list_machine->parameters.begin(), list_machine->parameters.end(), numeric_sort);
+            }
+            else {
+                StringValueSorter string_sort;
+                std::sort(list_machine->parameters.begin(), list_machine->parameters.end(), string_sort);
+            }
         }
         
         status = Complete;
