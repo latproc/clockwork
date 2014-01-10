605cd61c88da60cb3e509577b7429e5626232316
mleadbeater <mleadbeater@me.com>
2014-01-10 23:36:08 +1030

    updated to 1ksample / sec

--- iod/iod.cpp
+++ iod/iod.cpp
@@ -523,7 +523,7 @@ int main (int argc, char const *argv[])
 		return load_result;
 	}
 	
-	ECInterface::FREQUENCY=500;
+	ECInterface::FREQUENCY=1000;
 
 #ifndef EC_SIMULATOR
 	collectSlaveConfig(true);
