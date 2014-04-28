1467a7d16ba0c8c7dac7c08fcebdbf6677d8e850
Martin <mleadbeater@me.com>
2014-04-28 17:09:18 +0930

    ensure cw ignores SIGPIPE in case the remote raw socket closes unexpectedly

--- iod/cw.cpp
+++ iod/cw.cpp
@@ -151,6 +151,8 @@ bool setup_signals()
     if (sigaction(SIGTERM, &sa, 0) || sigaction(SIGINT, &sa, 0)) {
         return false;
     }
+    sa.sa_handler = SIG_IGN;
+    if (sigaction(SIGPIPE, &sa, 0)) return false;
     return true;
 }
 
