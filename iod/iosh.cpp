fec6d7cdf8918eaa5f963d59f902cac76e8d7a1c
Martin <mleadbeater@me.com>
2015-01-21 13:10:56 +1030

    add a few more commands to the autocomplete command table for iosh

--- iod/iosh.cpp
+++ iod/iosh.cpp
@@ -261,15 +261,18 @@ void initialise_commands() {
     commands.push_back("DISABLE");
     commands.push_back("ENABLE");
     commands.push_back("GET");
+    commands.push_back("HELP");
     commands.push_back("LIST");
     commands.push_back("MESSAGES");
     commands.push_back("MODBUS");
     commands.push_back("PROPERTY");
     commands.push_back("RESUME");
+    commands.push_back("SCHEDULER");
     commands.push_back("SEND");
     commands.push_back("SET");
     commands.push_back("TRACING");
     commands.push_back("TOGGLE");
+    commands.push_back("QUIT");
 }
 
 int main(int argc, const char * argv[])
