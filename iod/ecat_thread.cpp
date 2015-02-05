48b27378820f52bbb75b6b95f565b57199bb24c7
Martin <mleadbeater@me.com>
2015-02-05 17:22:34 +1030

    add alternative timer for experimentation

--- iod/ecat_thread.cpp
+++ iod/ecat_thread.cpp
@@ -173,6 +173,7 @@ void EtherCATThread::operator()() {
 	if (rc == -1) { perror("enable rtc pie"); exit(1); }
 #endif
 	uint64_t then = nowMicrosecs();
+	ECInterface::instance()->setReferenceTime(then % 0x100000000);
 	
 	sync_sock = new zmq::socket_t(*MessagingInterface::getContext(), ZMQ_REP);
 	sync_sock->bind("inproc://ethercat_sync");
@@ -232,6 +233,7 @@ void EtherCATThread::operator()() {
 			gettimeofday(&then,0);
 #endif
 #endif
+			ECInterface::instance()->setReferenceTime(now % 0x100000000);
 			uint64_t period = 1000000/freq;
 			int n = 0;
 			if (!machine_is_ready) {
@@ -248,19 +250,22 @@ void EtherCATThread::operator()() {
 				uint32_t ref_time =  ECInterface::instance()->getReferenceTime();
 				if (ref_time) {
 					uint32_t last_ref32 = last_ref_time % 0x100000000;
-					uint64_t delta_ref = 0;
+					int64_t delta_ref = 0;
 					if (last_ref32 > ref_time) {
 						//std::cerr << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n";
 						delta_ref = 0x100000000 + (uint64_t)ref_time;
-						//std::cerr << std::hex << std::setw(16) << last_ref32 << std::dec << "\n";
-						//std::cerr << std::hex << std::setw(16) << delta_ref << std::dec << "\n";
 						//std::cerr << std::hex << std::setw(8) << ref_time << std::dec << "\n";
+						//std::cerr << std::hex << std::setw(16) << delta_ref << std::dec << "\n";
+						//std::cerr << std::hex << std::setw(16) << last_ref32 << std::dec << "\n";
 						delta_ref -= last_ref32;
+						//std::cerr << std::hex << std::setw(16) << delta_ref << std::dec << "\n";
 					}
 					else
 						delta_ref = ref_time - last_ref_time;
-					if ( fabs((int64_t)delta_ref-period) > period*1000/10)
-						std::cerr << "cycle: " << delta_ref << " error: " << (int32_t)(delta_ref-2000000)<< "\n";
+					int64_t err = delta_ref - period;
+					if ( fabs(err) > (float)period/10)
+					std::cerr << "ref: " << ref_time 
+						<< " cycle: " << delta_ref << " error: " << err << "\n";
 					last_ref_time += delta_ref;
 				}
 #endif
