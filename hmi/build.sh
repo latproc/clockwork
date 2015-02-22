339eb8e5225c7c035c23a5af66b4beb23a28124c
mleadbeater <mleadbeater@me.com>
2015-02-23 02:46:40 +1030

    initial basic panel using fltk

---
+++ hmi/build.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+gcc -o panel -I /usr/local/include/modbus -L/usr/local/lib -lmodbus  panel.c
+g++ -o modbus_sample -I /usr/local/include -I/usr/local/include/modbus hello.cxx `fltk-config --ldflags` -L/usr/local/lib -lmodbus
