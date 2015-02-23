395a66cb0bb754d62020f020fc6c80bb8bb65028
mleadbeater <mleadbeater@me.com>
2015-02-23 16:41:24 +1030

    add a cmake configuration for easier build

--- hmi/build.sh
+++ hmi/build.sh
@@ -1,4 +1,5 @@
 #/bin/sh
 
 gcc -o panel -I /usr/local/include/modbus -L/usr/local/lib -lmodbus  panel.c
-g++ -o modbus_sample -I /usr/local/include -I/usr/local/include/modbus hello.cxx `fltk-config --ldflags` -L/usr/local/lib -lmodbus
+g++ -o modbus_sample -I /usr/local/include -I/usr/local/include/modbus modbus_sample.cxx \
+	`fltk-config --ldflags` -L/usr/local/lib -lmodbus -lboost_thread -lboost_system
