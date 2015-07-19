be62d31244887ac5fae6021d956c2f4dc5de723a
mleadbeater <mleadbeater@me.com>
2015-07-19 12:50:45 +0930

    add new file missing from the last commit

---
+++ iod/src/ControlSystemMachine.cpp
@@ -0,0 +1,28 @@
+/*
+  Copyright (C) 2012 Martin Leadbeater, Michael O'Connor
+
+  This file is part of Latproc
+
+  Latproc is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  Latproc is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Latproc; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <iostream>
+#include "IOComponent.h"
+#include "ControlSystemMachine.h"
+#include "LatprocConfig.h"
+
+ControlSystemMachine::ControlSystemMachine() : state(s_unknown) {
+	std::cout << "ControlSystem version " << Latproc_VERSION_MAJOR << "." << Latproc_VERSION_MINOR << "\n";
+}
