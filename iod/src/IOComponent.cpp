7af13264ffb16a665cdf1b7dd860fe8372ec0173
Martin Leadbeater <martin.leadbeater@gmail.com>
2015-07-24 07:42:33 +0930

    reworking poistion sensor filter

--- iod/src/IOComponent.cpp
+++ iod/src/IOComponent.cpp
@@ -386,10 +386,10 @@ public:
     bool property_changed;
     uint32_t noise_tolerance; // filter out changes with +/- this range
     LongBuffer positions;
-    uint32_t last_sent; // this is the value to send unless the read value moves away from the mean
+    int32_t last_sent; // this is the value to send unless the read value moves away from the mean
     uint16_t buffer_len;
     
-    InputFilterSettings() :property_changed(true), noise_tolerance(8), positions(16), last_sent(0), buffer_len(16) { }
+    InputFilterSettings() :property_changed(true), noise_tolerance(2), positions(16), last_sent(0), buffer_len(16) { }
 };
 
 AnalogueInput::AnalogueInput(IOAddress addr) : IOComponent(addr) { 
@@ -401,7 +401,7 @@ int32_t AnalogueInput::filter(int32_t raw) {
         config->property_changed = false;
     }
     config->positions.append(raw);
-    uint32_t mean = (config->positions.average(config->buffer_len) + 0.5f);
+    int32_t mean = (config->positions.average(config->buffer_len) + 0.5f);
     if ( (uint32_t)abs(mean - config->last_sent) >= config->noise_tolerance) {
         config->last_sent = mean;
     }
