663f88e2c59011efb58c7e308e398b4b70943aa7
Martin Leadbeater <mleadbeater@me.com>
2015-08-14 09:28:06 +0800

    fix a typo that broke compiles

--- iod/src/ProcessingThread.cpp
+++ iod/src/ProcessingThread.cpp
@@ -68,6 +68,7 @@ extern bool program_done;
 extern bool machine_is_ready;
 extern Statistics *statistics;
 extern uint64_t client_watchdog_timer;
+uint64_t clockwork_watchdog_timer = 0;
 
 #if 0
 
@@ -468,7 +469,7 @@ void ProcessingThread::operator()()
 		{
 			curr_t = nowMicrosecs();
 
-			if ( client_watchdog_timer && labs(curr_t - client_watchdog_timer) > 125) {
+			if ( client_watchdog_timer && labs(curr_t - client_watchdog_timer) > 125000) {
 //				NB_MSG << "####################### WATCHDOG######################" << labs(curr_t - client_watchdog_timer) << "\n"<<std::flush;
 			}
 			systems_waiting = pollZMQItems(poll_wait, items, ecat_sync, resource_mgr, dispatch_sync, sched_sync, ecat_out);
