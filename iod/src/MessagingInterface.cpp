b7167bb2d6b7cb8c2b29cc2d2a44e6a7e4f5452d
mleadbeater <mleadbeater@me.com>
2018-10-21 12:15:37 +1030

    restore nowMicrosecs temporarily as it is used in some EtherCAT code

--- iod/src/MessagingInterface.cpp
+++ iod/src/MessagingInterface.cpp
@@ -49,6 +49,10 @@ zmq::context_t *MessagingInterface::zmq_context = 0;
 std::map<std::string, MessagingInterface *>MessagingInterface::interfaces;
 bool MessagingInterface::abort_all = false;
 
+uint64_t nowMicrosecs() {
+  return microsecs();
+}
+
 uint64_t nowMicrosecs(const struct timeval &now) {
 	return (uint64_t) now.tv_sec * 1000000L + (uint64_t)now.tv_usec;
 }
