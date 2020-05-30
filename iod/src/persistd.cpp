c0f718271fa8284ccf457ae1af41191636c8ab97
mleadbeater <mleadbeater@me.com>
2020-05-30 15:39:56 +0930

    source layout change in persistd

--- iod/src/persistd.cpp
+++ iod/src/persistd.cpp
@@ -52,26 +52,26 @@ zmq::socket_t *cmd_socket = 0;
 
 static void finish(int sig)
 {
-    struct sigaction sa;
-    sa.sa_handler = SIG_DFL;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sigaction(SIGTERM, &sa, 0);
-    sigaction(SIGINT, &sa, 0);
-    done = true;
+	struct sigaction sa;
+	sa.sa_handler = SIG_DFL;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGTERM, &sa, 0);
+	sigaction(SIGINT, &sa, 0);
+	done = true;
 	exit(0);
 }
 
 bool setup_signals()
 {
-    struct sigaction sa;
-    sa.sa_handler = finish;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    if (sigaction(SIGTERM, &sa, 0) || sigaction(SIGINT, &sa, 0)) {
-        return false;
-    }
-    return true;
+	struct sigaction sa;
+	sa.sa_handler = finish;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	if (sigaction(SIGTERM, &sa, 0) || sigaction(SIGINT, &sa, 0)) {
+			return false;
+	}
+	return true;
 }
 
 // monitor the connection to iod and each time the connection
@@ -81,9 +81,9 @@ static bool need_refresh = false;
 
 class SetupConnectMonitor : public EventResponder {
 public:
-    void operator()(const zmq_event_t &event_, const char* addr_) {
-        need_refresh = true;
-    }
+	void operator()(const zmq_event_t &event_, const char* addr_) {
+			need_refresh = true;
+	}
 };
 
 class SetupDisconnectMonitor : public EventResponder {
@@ -105,92 +105,90 @@ public:
 
 bool loadActiveData(PersistentStore &store, const char *initial_settings)
 {
-    std::cout << "loading data from clockwork\n";
-		ExitMessage em("load data done");
-    cJSON *obj = cJSON_Parse(initial_settings);
-    if (!obj) return false;
-    int num_entries = cJSON_GetArraySize(obj);
-    if (num_entries)
-    {
-        // persitent data is provided as an array of object,
-        // each of which is
-        // (string name, string property, Value value)
-        for (int i=0; i<num_entries; ++i)
-        {
-            cJSON *item = cJSON_GetArrayItem(obj, i);
-            if (item->type == cJSON_Array)
-            {
-                cJSON *fld = cJSON_GetArrayItem(item, 0);
-                assert(fld->type == cJSON_String);
-                Value machine = fld->valuestring;
+	std::cout << "loading data from clockwork\n";
+	ExitMessage em("load data done");
+	cJSON *obj = cJSON_Parse(initial_settings);
+	if (!obj) return false;
+	int num_entries = cJSON_GetArraySize(obj);
+	if (num_entries)
+	{
+		// persitent data is provided as an array of object,
+		// each of which is
+		// (string name, string property, Value value)
+		for (int i=0; i<num_entries; ++i)
+		{
+			cJSON *item = cJSON_GetArrayItem(obj, i);
+			if (item->type == cJSON_Array)
+			{
+				cJSON *fld = cJSON_GetArrayItem(item, 0);
+				assert(fld->type == cJSON_String);
+				Value machine = fld->valuestring;
 
-                fld = cJSON_GetArrayItem(item, 1);
-                assert(fld->type == cJSON_String);
-                Value property = fld->valuestring;
+				fld = cJSON_GetArrayItem(item, 1);
+				assert(fld->type == cJSON_String);
+				Value property = fld->valuestring;
 
-                if (!ignored_properties.count(property.asString())) {
-                    cJSON *json_val = cJSON_GetArrayItem(item, 2);
-                    switch (json_val->type) {
-                        case cJSON_String:
-                            store.insert(machine.asString(), property.asString(), json_val->valuestring);
-                            break;
-											case cJSON_Number:
-														if (json_val->valueNumber.kind == cJSON_Number_int_t)
-																	store.insert(machine.asString(), property.asString(), json_val->valueNumber.val._int);
-														else
-                            	store.insert(machine.asString(), property.asString(), json_val->valueNumber.val._double);
-                            break;
-                        case cJSON_True:
-                            store.insert(machine.asString(), property.asString(), true);
-                            break;
-                        case cJSON_False:
-                            store.insert(machine.asString(), property.asString(), false);
-                            break;
-                        default:
-                            assert(false);
-                    }
-                }
-            }
-            else
-            {
-                char *node = cJSON_Print(item);
-                std::cerr << "item " << i << " is not of the expected format: " << node << "\n";
-                free(node);
-            }
-        }
-        return true;
-    }
-    else return true;
+				if (!ignored_properties.count(property.asString())) {
+					cJSON *json_val = cJSON_GetArrayItem(item, 2);
+					switch (json_val->type) {
+						case cJSON_String:
+							store.insert(machine.asString(), property.asString(), json_val->valuestring);
+							break;
+						case cJSON_Number:
+							if (json_val->valueNumber.kind == cJSON_Number_int_t)
+								store.insert(machine.asString(), property.asString(), json_val->valueNumber.val._int);
+							else
+								store.insert(machine.asString(), property.asString(), json_val->valueNumber.val._double);
+							break;
+						case cJSON_True:
+							store.insert(machine.asString(), property.asString(), true);
+							break;
+						case cJSON_False:
+							store.insert(machine.asString(), property.asString(), false);
+							break;
+						default:
+							assert(false);
+					}
+				}
+			}
+			else
+			{
+				char *node = cJSON_Print(item);
+				std::cerr << "item " << i << " is not of the expected format: " << node << "\n";
+				free(node);
+			}
+		}
+		return true;
+	}
+	else return true;
 }
 
 
 void CollectPersistentStatus(PersistentStore &store) {
-    std::string initial_settings;
+	std::string initial_settings;
 	int tries = 3;
-    do
-    {
+	do
+	{
 		std::cout << "-------- Collecting IO Status ---------\n" << std::flush;
-        if (cmd_socket && sendMessage("PERSISTENT STATUS", *cmd_socket, initial_settings)) {
-            if ( strncasecmp(initial_settings.c_str(), "ignored", strlen("ignored")) != 0)
-            {
-                if (loadActiveData(store, initial_settings.c_str())) store.save();
-                break;
-            }
-            else
-                sleep(2);
-        }
-        else {
-            sleep(1);
-        }
+		if (cmd_socket && sendMessage("PERSISTENT STATUS", *cmd_socket, initial_settings)) {
+			if ( strncasecmp(initial_settings.c_str(), "ignored", strlen("ignored")) != 0)
+			{
+				if (loadActiveData(store, initial_settings.c_str())) store.save();
+				break;
+			}
+			else
+				sleep(2);
+		}
+		else {
+				sleep(1);
+		}
 		if (--tries <= 0) {
 			NB_MSG << "failed to collect status, exiting to reconnect\n";
 			exit(0);
 		}
-    }
-    while (!done);
+	}
+	while (!done);
 }
-
-
 
 int main(int argc, const char * argv[]) {
 	char *pn = strdup(argv[0]);
@@ -200,117 +198,117 @@ int main(int argc, const char * argv[]) {
 	zmq::context_t context;
 	MessagingInterface::setContext(&context);
     
-    ignored_properties.insert("tab");
-    ignored_properties.insert("type");
-    ignored_properties.insert("name");
-    ignored_properties.insert("image");
-    ignored_properties.insert("class");
-    ignored_properties.insert("state");
-    ignored_properties.insert("export");
-    ignored_properties.insert("startup_enabled");
-    ignored_properties.insert("NAME");
-    ignored_properties.insert("STATE");
-    ignored_properties.insert("PERSISTENT");
-    ignored_properties.insert("POLLING_DELAY");
-    ignored_properties.insert("TRACEABLE");
+	ignored_properties.insert("tab");
+	ignored_properties.insert("type");
+	ignored_properties.insert("name");
+	ignored_properties.insert("image");
+	ignored_properties.insert("class");
+	ignored_properties.insert("state");
+	ignored_properties.insert("export");
+	ignored_properties.insert("startup_enabled");
+	ignored_properties.insert("NAME");
+	ignored_properties.insert("STATE");
+	ignored_properties.insert("PERSISTENT");
+	ignored_properties.insert("POLLING_DELAY");
+	ignored_properties.insert("TRACEABLE");
 	ignored_properties.insert("IOTIME");
 
-    po::options_description desc("Allowed options");
-    desc.add_options()
-    ("help", "produce help message")
-    ("verbose", "display changes on stdout")
-    ;
-    po::variables_map vm;        
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);    
-    if (vm.count("help")) {
-        std::cout << desc << "\n";
-        return 1;
-    }
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help", "produce help message")
+		("verbose", "display changes on stdout")
+	;
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+	if (vm.count("help")) {
+		std::cout << desc << "\n";
+		return 1;
+	}
 
-    bool verbose = false;
-    if (vm.count("verbose")) verbose = true;
-    
-    setup_signals();
+	bool verbose = false;
+	if (vm.count("verbose")) verbose = true;
 
-    PersistentStore store("persist.dat");
-    //store.load(); // disabled load store since we take it from clockwork now
-    
-    SubscriptionManager subscription_manager("PERSISTENCE_CHANNEL", eCLOCKWORK);
-    SetupConnectMonitor connect_responder;
-    SetupDisconnectMonitor disconnect_responder;
-    cmd_socket = &subscription_manager.setup();
-    subscription_manager.monit_setup->addResponder(ZMQ_EVENT_CONNECTED, &connect_responder);
-    subscription_manager.monit_setup->addResponder(ZMQ_EVENT_DISCONNECTED, &disconnect_responder);
-    
-    while (!done) {
-        zmq::pollitem_t items[] = {
-            { (void*)subscription_manager.setup(), 0, ZMQ_POLLERR | ZMQ_POLLIN, 0 },
-            { (void*)subscription_manager.subscriber(), 0, ZMQ_POLLERR | ZMQ_POLLIN, 0 },
-        };
+	setup_signals();
+
+	PersistentStore store("persist.dat");
+	//store.load(); // disabled load store since we take it from clockwork now
+
+	SubscriptionManager subscription_manager("PERSISTENCE_CHANNEL", eCLOCKWORK);
+	SetupConnectMonitor connect_responder;
+	SetupDisconnectMonitor disconnect_responder;
+	cmd_socket = &subscription_manager.setup();
+	subscription_manager.monit_setup->addResponder(ZMQ_EVENT_CONNECTED, &connect_responder);
+	subscription_manager.monit_setup->addResponder(ZMQ_EVENT_DISCONNECTED, &disconnect_responder);
+
+	while (!done) {
+		zmq::pollitem_t items[] = {
+			{ (void*)subscription_manager.setup(), 0, ZMQ_POLLERR | ZMQ_POLLIN, 0 },
+			{ (void*)subscription_manager.subscriber(), 0, ZMQ_POLLERR | ZMQ_POLLIN, 0 },
+		};
 		if (!subscription_manager.checkConnections()) {
 			usleep(100);
 			continue;
 		}
-        try {
-            int rc = zmq::poll( &items[0], 2, 500);
-            if (need_refresh) {
-                CollectPersistentStatus(store);
-                need_refresh = false;
-            }
-            if (rc == 0) continue;
-        }
-        catch(zmq::error_t e) {
-            if (errno == EINTR || errno == EAGAIN) continue;
-        }
-        if ( !(items[1].revents & ZMQ_POLLIN) ) continue;
-        //zmq::message_t update;
-        char data[1000];
-        size_t len = 0;
-        try {
-            len = subscription_manager.subscriber().recv(data, 1000, ZMQ_DONTWAIT);
-            if (!len) continue;
-        }
-        catch (zmq::error_t e) {
-            if (errno == EINTR) continue;
-            
-        }
-        data[len] = 0;
+		try {
+			int rc = zmq::poll( &items[0], 2, 500);
+			if (need_refresh) {
+				CollectPersistentStatus(store);
+				need_refresh = false;
+			}
+			if (rc == 0) continue;
+		}
+		catch(zmq::error_t e) {
+				if (errno == EINTR || errno == EAGAIN) continue;
+		}
+		if ( !(items[1].revents & ZMQ_POLLIN) ) continue;
+		//zmq::message_t update;
+		char data[1000];
+		size_t len = 0;
+		try {
+			len = subscription_manager.subscriber().recv(data, 1000, ZMQ_DONTWAIT);
+			if (!len) continue;
+		}
+		catch (zmq::error_t e) {
+				if (errno == EINTR) continue;
 
-        if (verbose) std::cout << data << "\n";
+		}
+		data[len] = 0;
 
-        try {
-            std::string cmd;
-            std::list<Value> *param_list = 0;
-            if (MessageEncoding::getCommand(data, cmd, &param_list)) {
-                if (cmd == "PROPERTY" && param_list && param_list->size() == 3) {
-                    std::string property;
-                    std::list<Value>::const_iterator iter = param_list->begin();
-                    Value machine_name = *iter++;
-                    Value property_name = *iter++;
-                    Value value = *iter++;
-                    store.insert(machine_name.asString(), property_name.asString(), value);
-                    store.save();
-                }
-						else
-							std::cerr << "unexpected command: " << cmd << " sent to persistd\n";
-            }
-            else {
-                std::istringstream iss(data);
-                std::string property, op, value;
-                iss >> property >> op >> value;
-                std::string machine_name;
-                store.split(machine_name, property);
-                store.insert(machine_name, property, value.c_str());
-                store.save();
-            }
-						if (param_list) { delete param_list; param_list = 0; }
-        }
-        catch(std::exception e) {
-            std::cerr << "exception " <<e.what() << " processing: " << data << "\n";
-        }
-    }
+		if (verbose) std::cout << data << "\n";
 
-    return 0;
+		try {
+				std::string cmd;
+				std::list<Value> *param_list = 0;
+				if (MessageEncoding::getCommand(data, cmd, &param_list)) {
+					if (cmd == "PROPERTY" && param_list && param_list->size() == 3) {
+						std::string property;
+						std::list<Value>::const_iterator iter = param_list->begin();
+						Value machine_name = *iter++;
+						Value property_name = *iter++;
+						Value value = *iter++;
+						store.insert(machine_name.asString(), property_name.asString(), value);
+						store.save();
+					}
+				else
+					std::cerr << "unexpected command: " << cmd << " sent to persistd\n";
+				}
+				else {
+						std::istringstream iss(data);
+						std::string property, op, value;
+						iss >> property >> op >> value;
+						std::string machine_name;
+						store.split(machine_name, property);
+						store.insert(machine_name, property, value.c_str());
+						store.save();
+				}
+				if (param_list) { delete param_list; param_list = 0; }
+		}
+		catch(std::exception e) {
+				std::cerr << "exception " <<e.what() << " processing: " << data << "\n";
+		}
+	}
+
+	return 0;
 }
 
