e927d0e8ac7311374511ac5c264c26cf7336b7b1
mleadbeater <mleadbeater@me.com>
2020-09-06 15:17:43 +0930

    update ethercat startup script to use ip commad rather than ifconfig

--- scripts/ethercat.sh
+++ scripts/ethercat.sh
@@ -7,19 +7,19 @@ else
 	IF=$2
 fi
 
-MAC=`ifconfig $IF |grep -E -o '([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}'`
+MAC=`ip link show $IF |grep -E -o '([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}'`
 
 echo "using interface $IF, hwaddr $MAC"
 
 if [ "$1" = "stop" ]; then
-	ifconfig $IF down
+	ip link set dev $IF down
 	rmmod ec_generic
 	rmmod ec_master
 	modprobe e1000e InterruptThrottleRate=0
 elif [ "$1" = "start" ]; then
 	modprobe ec_master main_devices=$MAC
 	modprobe ec_generic
-	ifconfig $IF up
+	ip link set dev $IF up
 	ethtool -C $IF rx-usecs 0 rx-frames 1 tx-usecs 0 tx-frames 1
 	[ -e /dev/EtherCAT0 ] && chmod go+rw /dev/EtherCAT0
 fi
