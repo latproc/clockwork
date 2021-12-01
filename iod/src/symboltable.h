3c4daa9ea698174b47a4b22e188fd5e6ea8c3906
Martin Leadbeater <mleadbeater@me.com>
2021-12-01 23:53:59 +1030

    fixed a bug rendering timestamps, added LOCALTIME and UTCTIME, updated example

--- iod/src/symboltable.h
+++ iod/src/symboltable.h
@@ -7,7 +7,7 @@
   modify it under the terms of the GNU General Public License
   as published by the Free Software Foundation; either version 2
   of the License, or (at your option) any later version.
-  
+
   Latproc is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -76,10 +76,10 @@ public:
 };
 
 class SymbolTable {
-public: 
+public:
 	SymbolTable();
 	enum ReplaceMode { ST_REPLACE, NO_REPLACE };
-	
+
     bool add(const char *name, const Value &val, ReplaceMode replace_mode = ST_REPLACE);
     bool add(const std::string name, const Value &val, ReplaceMode replace_mode = ST_REPLACE);
     void add(const SymbolTable &orig, ReplaceMode replace_mode = ST_REPLACE); // load symbols optionally with replacement
@@ -91,22 +91,22 @@ public:
     bool exists(const char *name);
     bool exists(int token_id);
     void clear();
-    
+
     SymbolTableConstIterator begin() const {return st.begin(); }
     SymbolTableConstIterator end() const { return st.end(); }
     void push_back(std::pair<std::string, Value>item) { st[item.first] = item.second; }
-    
+
     SymbolTable(const SymbolTable &orig);
     SymbolTable &operator=(const SymbolTable &orig);
-    
+
     std::ostream &operator <<(std::ostream & out) const;
     size_t size() const { return st.size(); }
 	bool empty() const { return st.empty(); }
-    
+
     static bool isKeyword(const Value &name);
     static bool isKeyword(const char *name);
     static const Value &getKeyValue(const char *name);
-    
+
 	static const Value Null;
 	static const Value True;
 	static const Value False;
