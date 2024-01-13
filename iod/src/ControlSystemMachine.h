4dcbcaeb60b0aed19c85439b8a463fa1ef4ffe6b
Martin Leadbeater <mleadbeater@me.com>
2024-01-14 09:50:38 +1030

    General header fixes and minor cleanup
    value.h: Fix includes
    ControlSystemMachine.h: Fix includes

--- iod/src/ControlSystemMachine.h
+++ iod/src/ControlSystemMachine.h
@@ -17,6 +17,7 @@
     along with Latproc; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+#pragma once
 
 #include "ECInterface.h"
 #include "IOComponent.h"
--- iod/src/clockwork.h
+++ iod/src/clockwork.h
@@ -23,12 +23,15 @@
 
 #include <list>
 #include <string>
+#include <value.h>
 
 class MachineInstance;
 class Value;
 
 extern std::list<std::string> error_messages;
 extern int num_errors;
+
+class MachineInstance;
 
 int loadOptions(int argc, char const *argv[], std::list<std::string> &files);
 int loadConfig(std::list<std::string> &files);
