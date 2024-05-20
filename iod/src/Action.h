c4f0834d9397a8534f60b6d7b3004737e08ce78e
Martin Leadbeater <mleadbeater@me.com>
2024-05-20 17:39:24 +0930

    Split Trigger out of the Action.h/cpp

--- iod/src/Action.h
+++ iod/src/Action.h
@@ -18,11 +18,11 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-#ifndef cwlang_Action_h
-#define cwlang_Action_h
+#pragma once
 
 #include "Expression.h"
 #include "Message.h"
+#include "Trigger.h"
 #include <boost/foreach.hpp>
 #include <cstdlib>
 #include <cstring>
@@ -45,59 +45,6 @@ class ActionTemplate {
 };
 std::ostream &operator<<(std::ostream &out, const ActionTemplate &a);
 
-class TriggerInternals;
-class Trigger;
-struct TriggerOwner {
-    virtual ~TriggerOwner() = default;
-    virtual void triggerFired(Trigger *trigger) {};
-};
-
-class Trigger {
-  public:
-    explicit Trigger(const std::string &n);
-    Trigger(TriggerOwner *, const std::string &n);
-
-    virtual ~Trigger();
-    Trigger *retain();
-    virtual Trigger *release();
-    static char *getTriggers();
-    void addHolder(Action *h);
-    void removeHolder(Action *h);
-
-    void setOwner(TriggerOwner *new_owner);
-    bool enabled() const;
-    bool fired() const;
-    void fire();
-    void disable();
-    virtual const std::string &getName() const;
-    //const std::string &getName();
-    bool matches(const std::string &event);
-
-    uint64_t startTime();
-    void report(const char *message);
-    int getRefs() const { return refs; }
-
-    std::ostream &operator<<(std::ostream &out) const;
-
-  protected:
-    TriggerInternals *_internals;
-    std::string name;
-    bool seen;
-    TriggerOwner *owner;
-    bool deleted;
-    int refs;
-
-  private:
-    bool is_active;
-
-  public:
-    // None of these constructors and assignment operators are
-    // implemented because they are not supported
-    Trigger() = delete;
-    Trigger(const Trigger &o) = delete;
-    Trigger &operator=(const Trigger &o) = delete;
-};
-std::ostream &operator<<(std::ostream &out, const Trigger &t);
 
 // an action is started by operator(). If the action successfully starts,
 // running() will return true. When the action is complete, complete() will
@@ -226,4 +173,4 @@ class TriggeredAction : public Action {
 
 std::ostream &operator<<(std::ostream &out, const Action &);
 
-#endif
+
---
+++ iod/src/Trigger.h
@@ -0,0 +1,89 @@
+/*
+    Copyright (C) 2012 Martin Leadbeater, Michael O'Connor
+
+    This file is part of Latproc
+
+    Latproc is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    Latproc is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Latproc; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#pragma once
+
+#include "Expression.h"
+#include "Message.h"
+#include <boost/foreach.hpp>
+#include <cstdlib>
+#include <cstring>
+#include <set>
+#include <sys/types.h>
+#include <vector>
+
+typedef std::vector<std::string> ActionParameterList;
+class MachineInstance;
+
+class Action;
+class TriggerInternals;
+class Trigger;
+
+struct TriggerOwner {
+    virtual ~TriggerOwner() = default;
+    virtual void triggerFired(Trigger *trigger) {};
+};
+
+class Trigger {
+  public:
+    explicit Trigger(const std::string &n);
+    Trigger(TriggerOwner *, const std::string &n);
+
+    virtual ~Trigger();
+    Trigger *retain();
+    virtual Trigger *release();
+    static char *getTriggers();
+    void addHolder(Action *h);
+    void removeHolder(Action *h);
+
+    void setOwner(TriggerOwner *new_owner);
+    bool enabled() const;
+    bool fired() const;
+    void fire();
+    void disable();
+    virtual const std::string &getName() const;
+    //const std::string &getName();
+    bool matches(const std::string &event);
+
+    uint64_t startTime();
+    void report(const char *message);
+    int getRefs() const { return refs; }
+
+    std::ostream &operator<<(std::ostream &out) const;
+
+  protected:
+    TriggerInternals *_internals;
+    std::string name;
+    bool seen;
+    TriggerOwner *owner;
+    bool deleted;
+    int refs;
+
+  private:
+    bool is_active;
+
+  public:
+    // None of these constructors and assignment operators are
+    // implemented because they are not supported
+    Trigger() = delete;
+    Trigger(const Trigger &o) = delete;
+    Trigger &operator=(const Trigger &o) = delete;
+};
+std::ostream &operator<<(std::ostream &out, const Trigger &t);
