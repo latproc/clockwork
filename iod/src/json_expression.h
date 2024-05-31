9e1d5fd3ee5bdd5faecd1e0faa30acf2e5a8742f
Martin Leadbeater <mleadbeater@me.com>
2024-05-31 18:33:20 +0930

    Add support for updating values within JSON objects

--- iod/src/json_expression.h
+++ iod/src/json_expression.h
@@ -1,18 +1,25 @@
 #pragma once
 
 #include <cJSON.h>
-#include <string>
 #include <iostream>
+#include <string>
+#include <value.h>
 
 cJSON *apply(const std::string &str, cJSON *json);
+cJSON *assign(const std::string &str, cJSON *json, const std::string &value);
+cJSON *assign(const std::string &str, cJSON *json, int value);
+cJSON *assign(const std::string &str, cJSON *json, uint64_t value);
+cJSON *assign(const std::string &str, cJSON *json, const Value &value);
+cJSON *assign(const std::string &str, cJSON *json, double value);
+cJSON *assign(const std::string &str, cJSON *json, cJSON *value);
+cJSON *assign(const std::string &str, cJSON *json, bool value);
 
 class JsonExpr {
-public:
+  public:
     JsonExpr(const char *str) : expr(str) {}
-    JsonExpr(const std::string & str) : expr{str} {}
-    cJSON *apply(cJSON *json) const {
-        return ::apply(expr, json);
-    }
-private:
+    JsonExpr(const std::string &str) : expr{str} {}
+    cJSON *apply(cJSON *json) const { return ::apply(expr, json); }
+
+  private:
     std::string expr;
 };
