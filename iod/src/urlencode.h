c90fab8ffd5d1e2baa768110fb8f41fbefd6374a
Martin Leadbeater <mleadbeater@me.com>
2024-11-10 18:53:56 +1030

    Add missing urlencode files

---
+++ iod/src/urlencode.h
@@ -0,0 +1,9 @@
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <cJSON.h>
+#include <tl/expected.hpp>
+
+std::string url_encode(const std::string &str);
+tl::expected<std::string, std::string> url_encode_json(cJSON *json);
+
