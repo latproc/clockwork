0c7110b513b16f67d0ec54ade08703d62edb0c3e
Martin Leadbeater <mleadbeater@me.com>
2024-11-17 08:28:44 +1030

    MachineInstance.cpp: Add the machine's owner to the output of DESCRIBE

--- iod/src/MachineInstance.cpp
+++ iod/src/MachineInstance.cpp
@@ -767,8 +767,11 @@ void MachineInstance::describe(std::ostream &out) {
         for (unsigned int i = 0; i < parameters.size(); i++) {
             Value p_i = parameters[i].val;
             if (p_i.kind == Value::t_symbol) {
+                auto param_class = parameters[i].machine;
                 out << "  parameter " << (i + 1) << " " << p_i.sValue << " ("
-                    << parameters[i].real_name << "), state: "
+                    << parameters[i].real_name
+                    << ": " << (param_class && param_class->state_machine ? param_class->state_machine->name : "undefined")
+                    << "), state: "
                     << (parameters[i].machine ? parameters[i].machine->getCurrent().getName() : "")
                     << (parameters[i].machine && !parameters[i].machine->enabled() ? " DISABLED"
                                                                                    : "")
