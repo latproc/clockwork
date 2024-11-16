aa06b8ad9e0c206be75a23315807b3963c9944fc
Martin Leadbeater <mleadbeater@me.com>
2024-11-17 00:30:01 +1030

    Fix some signed/unsigned issues that generated compiler warnings

--- iod/src/IOComponent.cpp
+++ iod/src/IOComponent.cpp
@@ -705,7 +705,7 @@ class InputFilterSettings {
         }
         double c[] = {0.081, 0.215, 0.541, 0.865, 1, 0.865, 0.541, 0.215, 0.081};
         double res = 0;
-        for (size_t i = 0; i < filter_length; ++i) {
+        for (int64_t i = 0; i < filter_length; ++i) {
             double f = (double)getBufferValue(positions, i);
             //printf(" %.3f,%.3f ",f, f*c[i]);
             res += f * c[i];
