69febe837b1ffbdefc0487a9b9276f160b525a50
Martin Leadbeater <mleadbeater@me.com>
2024-11-16 22:58:14 +1030

    Add bit operations and tests

--- iod/src/bit_ops.cpp
+++ iod/src/bit_ops.cpp
@@ -1,8 +1,32 @@
 #include "bit_ops.h"
 #include <inttypes.h>
+#include <sstream>
+#include <iomanip>
+#include <stdint.h>
+
+std::string buffer_to_string(const uint8_t *p, size_t len) {
+    std::stringstream ss;
+    for (size_t i = 0; i < len; ++i) {
+        ss << std::setw(2) << std::setfill('0') << std::hex << (unsigned int)p[i]
+                  << std::dec;
+    }
+    return ss.str();
+}
+
+std::string buffer_to_string(const std::vector<uint8_t> &buf) {
+    return buffer_to_string(buf.data(), buf.size());
+}
+
 #if VERBOSE_DEBUG
-#include <iostream>
-void display(uint8_t *, size_t len);
+void show_buffers(uint8_t *dest, uint8_t *src, uint8_t *mask, size_t len) {
+    std::cout << "copying masked bits: \n";
+    std::cout << buffer_to_string(dest, len);
+    std::cout << "\n";
+    std::cout << buffer_to_string(src, len);
+    std::cout << "\n";
+    std::cout << buffer_to_string(mask, len);
+    std::cout << "\n";
+}
 #endif
 
 void set_bit(uint8_t *q, unsigned int bitpos, unsigned int val) {
@@ -15,37 +39,141 @@ void set_bit(uint8_t *q, unsigned int bitpos, unsigned int val) {
     }
 }
 
-void copyMaskedBits(uint8_t *dest, uint8_t *src, uint8_t *mask, size_t len) {
+// applies mask to src and returns init + masked values from src
+uint8_t copy_masked_bits(uint8_t init, uint8_t src, uint8_t mask) {
+    uint8_t result = init;
+    uint8_t bitmask = 0x80;
+    while (bitmask) {
+        if (mask & bitmask) {
+            if (src & bitmask) {
+                result |= bitmask;
+            }
+            else {
+                result &= (uint8_t)(0xff - bitmask);
+            }
+        }
+        bitmask = bitmask >> 1;
+    }
+    return result;
+}
 
-#if VERBOSE_DEBUG
-    std::cout << "copying masked bits: \n";
-    display(dest, len);
-    std::cout << "\n";
-    display(src, len);
-    std::cout << "\n";
-    display(mask, len);
-    std::cout << "\n";
-#endif
+void copyMaskedBits(uint8_t *dest, const uint8_t *src, const uint8_t *mask, size_t len) {
     size_t count = len;
     while (count--) {
-        uint8_t bitmask = 0x80;
-        for (int i = 0; i < 8; ++i) {
-            if (*mask & bitmask) {
-                if (*src & bitmask) {
-                    *dest |= bitmask;
-                }
-                else {
-                    *dest &= (uint8_t)(0xff - bitmask);
-                }
-            }
-            bitmask = bitmask >> 1;
-        }
+        if (*mask) { *dest = copy_masked_bits(*dest, *src, *mask); }
         ++src;
         ++dest;
         ++mask;
     }
-#if VERBOSE_DEBUG
-    display(result, len);
-    std::cout << "\n";
+}
+
+// applies mask to src and returns init + masked values from src
+uint8_t copy_masked_bits(uint8_t dest, uint8_t src, uint8_t mask,
+                uint8_t last, uint8_t &change_mask, size_t &change_count) {
+    uint8_t result = dest;
+    uint8_t bitmask = 0x80;
+    change_mask = 0x00;
+    while (bitmask) {
+        if (mask & bitmask) {
+            if ((src & bitmask) != (last & bitmask)) {
+                change_mask |= bitmask;
+                ++change_count;
+            }
+            if (src & bitmask) {
+                result |= bitmask;
+            }
+            else {
+                result &= (uint8_t)(0xff - bitmask);
+            }
+        }
+        bitmask = bitmask >> 1;
+    }
+    return result;
+}
+// Applies mask to src and last and compares results.
+// Where there is a difference a bit is set in the change mask.
+// mask applied to dest and masked bits are set to match src
+size_t copyMaskedBitsAndSetChangeMask(uint8_t *dest, uint8_t *change_mask, const uint8_t *src, const uint8_t *mask, const uint8_t *last, size_t len) {
+    size_t num_changes = 0;
+    size_t count = len;
+    while (count--) {
+        if (*mask) {
+            *dest = copy_masked_bits(*dest, *src, *mask, *last, *change_mask, num_changes);
+#if 0
+            uint8_t bitmask = 0x80;
+            while (bitmask) {
+                if (*mask & bitmask) {
+                    if ((*src & bitmask) != (*last & bitmask)) {
+                        *change_mask |= bitmask;
+                        ++num_changes;
+                    }
+                    if (*src & bitmask) {
+                        *dest |= bitmask;
+                    }
+                    else {
+                        *dest &= (uint8_t)(0xff - bitmask);
+                    }
+                }
+                bitmask = bitmask >> 1;
+            }
 #endif
+        }
+        ++src;
+        ++dest;
+        ++mask;
+        ++last;
+        ++change_mask;
+    }
+    return num_changes;
+}
+
+ChangeSet copyMaskedBitsAndReturnMaskOfChanges(uint8_t *dest, const uint8_t *src, const uint8_t *mask, const uint8_t *last, size_t len) {
+    ChangeSet cs;
+    cs.changes.resize(len);
+    uint8_t *change_ptr = cs.changes.data();
+    size_t count = len;
+    while (count--) {
+        uint8_t bitmask = 0x80;
+        if (*mask) {
+            *dest = copy_masked_bits(*dest, *src, *mask, *last, *change_ptr, cs.count);
+#if 0
+            while (bitmask) {
+                if (*mask & bitmask) {
+                    if ((*src & bitmask) != (*last & bitmask)) {
+                        *change_ptr |= bitmask;
+                        ++cs.count;
+                    }
+                    if (*src & bitmask) {
+                        *dest |= bitmask;
+                    }
+                    else {
+                        *dest &= (uint8_t)(0xff - bitmask);
+                    }
+                }
+                bitmask = bitmask >> 1;
+            }
+#endif
+        }
+        ++src;
+        ++dest;
+        ++mask;
+        ++last;
+        ++change_ptr;
+    }
+    return cs;
+}
+
+void set_mask_bits(unsigned int offset, unsigned int bitpos, unsigned int bitlen, uint8_t *result) {
+    offset += bitpos / 8;
+    bitpos = bitpos % 8;
+    uint8_t mask = 0x01 << bitpos;
+    // set  a bit in the mask for each bit of this value
+    for (unsigned int i = 0; i < bitlen; ++i) {
+        result[offset] |= mask;
+        mask = mask << 1;
+        if (!mask) {
+            mask = 0x01;
+            ++offset;
+        }
+    }
 }
--- iod/tests/test_bit_ops.cpp
+++ iod/tests/test_bit_ops.cpp
@@ -1,13 +1,14 @@
 #include "bit_ops.h"
 #include "library_globals.cpp"
 #include "gtest/gtest.h"
+#include <bits/stdint-uintn.h>
 
 // internal to IOComponent but not static..
 //void set_bit(uint8_t *q, unsigned int bitpos, unsigned int val);
 //void copyMaskedBits(uint8_t *dest, uint8_t*src, uint8_t *mask, size_t len);
 
 TEST(SetBitTest, SetsCorrectBit) {
-    uint8_t buf;
+    uint8_t buf = 0;
     set_bit(&buf, 0, 1);
     EXPECT_EQ(buf, 1);
     set_bit(&buf, 7, 1);
@@ -36,3 +37,97 @@ TEST(CopyMaskedBitsTest, CopiesMaskedBits) {
     EXPECT_EQ(255 - 4, dst[2]);
     EXPECT_EQ(9, dst[3]);
 }
+
+TEST(GetChangesTest, ReturnsZeroWhenThereAreNoChanges) {
+    uint8_t src[] = {255, 255, 0, 255};
+    uint8_t dst[] = {0, 0, 255, 0};
+    uint8_t mask[] = {1, 2, 4, 9};
+    uint8_t last[] = {255, 255, 0, 255};
+    auto changes = copyMaskedBitsAndReturnMaskOfChanges(dst, src, mask, last, 4);
+    EXPECT_EQ(0, changes.count);
+    for (size_t i = 0; i < 4; i++) {
+        EXPECT_EQ(0, changes.changes[i]);
+    }
+    uint8_t expected[] = {1, 2, 255 - 4, 9};
+    for (size_t i = 0; i < 4; i++) {
+        EXPECT_EQ(dst[i], expected[i]);
+    }
+}
+
+TEST(SetMaskedBitTest, DoesNotTouchUnmaskedBits) {
+    EXPECT_EQ(copy_masked_bits(0x23, 0xff, 0b00000000), 0x23);
+    EXPECT_EQ(copy_masked_bits(0x23, 0xff, 0b00000100), 0x27);
+    EXPECT_EQ(copy_masked_bits(0b11111011, 0x00, 0b00000011), 0b11111000);
+}
+
+TEST(GetChangesTest, ReturnsCorrectMaskedData) {
+    std::vector<uint8_t> src{255, 255, 0, 255};
+    std::vector<uint8_t> dst{0, 0, 255, 0};
+    std::vector<uint8_t> mask{1, 2, 4, 9};
+    std::vector<uint8_t> last{255, 255, 0, 255};
+    auto changes = copyMaskedBitsAndReturnMaskOfChanges(dst.data(), src.data(), mask.data(), last.data(), 4);
+    std::vector<uint8_t> expected{1, 2, 255 - 4, 9};
+    EXPECT_EQ(0, changes.count);
+    EXPECT_TRUE(expected ==  dst);
+}
+
+TEST(GetChangesTest, ReturnedValueIsIndependentOfPreviousValue) {
+    std::vector<uint8_t> src{255, 255, 0, 255};
+    std::vector<uint8_t> dst{0, 0, 255, 0};
+    std::vector<uint8_t> mask{1, 2, 4, 9};
+    std::vector<uint8_t> last{255, 255, 0, 255};
+    std::vector<uint8_t> expected{1, 2, 255 - 4, 9};
+    auto changes = copyMaskedBitsAndReturnMaskOfChanges(dst.data(), src.data(), mask.data(), last.data(), 4);
+    EXPECT_EQ(0, changes.count);
+    EXPECT_TRUE(expected ==  dst);
+    last[0] = 0;
+    changes = copyMaskedBitsAndReturnMaskOfChanges(dst.data(), src.data(), mask.data(), last.data(), 4);
+    EXPECT_EQ(1, changes.count);
+    EXPECT_TRUE(expected ==  dst);
+}
+
+TEST(GetChangesTest, ReturnsCorrectChanges) {
+    std::vector<uint8_t>  dst{0b00000000, 0b00000000, 0b11111111, 0b00000000};
+    std::vector<uint8_t>  src{0b11111111, 0b11111110, 0b00000000, 0b11111111};
+    std::vector<uint8_t> mask{0b00000001, 0b00000011, 0b00000110, 0b00001001};
+    std::vector<uint8_t> last{0b11111110, 0b11111101, 0b00000000, 0b11111000};
+    auto changes = copyMaskedBitsAndReturnMaskOfChanges(dst.data(), src.data(), mask.data(), last.data(), 4);
+    std::vector<uint8_t> expected{1, 2, 255 - 6, 9};
+    std::vector<uint8_t> expected_changes{1, 3, 0, 1};
+    EXPECT_EQ(4, changes.count);
+    EXPECT_TRUE(expected ==  dst);
+    EXPECT_TRUE(expected_changes == changes.changes);
+}
+
+TEST(GetChangesTest, InplaceVersionReturnsCorrectMask) {
+    std::vector<uint8_t> src{255, 255, 0, 255};
+    std::vector<uint8_t> dst{0, 0, 255, 0};
+    std::vector<uint8_t> mask{1, 2, 4, 9};
+    std::vector<uint8_t> last{255, 255, 0, 255};
+    std::vector<uint8_t> change_mask{1,2,3,4};
+    auto changes = copyMaskedBitsAndSetChangeMask(dst.data(), change_mask.data(),
+                    src.data(), mask.data(), last.data(), 4);
+    std::vector<uint8_t> expected{1, 2, 255 - 4, 9};
+    EXPECT_EQ(0, changes);
+    EXPECT_TRUE(expected ==  dst);
+}
+
+TEST(GetChangesTest, InplaceVersionReturnsCorrectChanges) {
+    std::vector<uint8_t>  dst{0b00000000, 0b00000000, 0b11111111, 0b00000000};
+    std::vector<uint8_t>  src{0b11111111, 0b11111110, 0b00000000, 0b11111111};
+    std::vector<uint8_t> mask{0b00000001, 0b00000011, 0b00000110, 0b00001001};
+    std::vector<uint8_t> last{0b11111110, 0b11111101, 0b00000000, 0b11111000};
+    std::vector<uint8_t> change_mask{1,2,3,4}; // initial data should be discarded
+    auto changes = copyMaskedBitsAndSetChangeMask(dst.data(), change_mask.data(),
+                    src.data(), mask.data(), last.data(), 4);
+    std::vector<uint8_t> expected{1, 2, 255 - 6, 9};
+    std::vector<uint8_t> expected_changes{1, 3, 0, 1};
+    EXPECT_EQ(4, changes);
+    EXPECT_TRUE(expected ==  dst);
+    if (expected_changes != change_mask) {
+        std::cout << buffer_to_string(expected_changes)
+                 << " " << buffer_to_string(change_mask) << "\n";
+    }
+    EXPECT_TRUE(expected_changes == change_mask);
+}
+
