bf7cff67b6d8152d8701c2d68e6e8fda55c24a3e
Martin Leadbeater <mleadbeater@me.com>
2024-11-16 22:58:14 +1030

    Add bit operations and tests

--- iod/src/IOComponent.cpp
+++ iod/src/IOComponent.cpp
@@ -23,6 +23,7 @@
 #include "Logger.h"
 #include "MachineInstance.h"
 #include "MessagingInterface.h"
+#include "bit_ops.h"
 #include <algorithm>
 #include <boost/thread/mutex.hpp>
 #include <iomanip>
@@ -122,52 +123,6 @@ void handle_io_sampling(uint64_t io_clock) {
 #if VERBOSE_DEBUG
 static void display(const uint8_t *p, unsigned int count = 0);
 #endif
-
-void set_bit(uint8_t *q, unsigned int bitpos, unsigned int val) {
-    uint8_t bitmask = 1 << bitpos;
-    if (val) {
-        *q |= bitmask;
-    }
-    else {
-        *q &= (uint8_t)(0xff - bitmask);
-    }
-}
-
-void copyMaskedBits(uint8_t *dest, uint8_t *src, uint8_t *mask, size_t len) {
-
-    uint8_t *result = dest;
-#if VERBOSE_DEBUG
-    std::cout << "copying masked bits: \n";
-    display(dest, len);
-    std::cout << "\n";
-    display(src, len);
-    std::cout << "\n";
-    display(mask, len);
-    std::cout << "\n";
-#endif
-    size_t count = len;
-    while (count--) {
-        uint8_t bitmask = 0x80;
-        for (int i = 0; i < 8; ++i) {
-            if (*mask & bitmask) {
-                if (*src & bitmask) {
-                    *dest |= bitmask;
-                }
-                else {
-                    *dest &= (uint8_t)(0xff - bitmask);
-                }
-            }
-            bitmask = bitmask >> 1;
-        }
-        ++src;
-        ++dest;
-        ++mask;
-    }
-#if VERBOSE_DEBUG
-    display(result, len);
-    std::cout << "\n";
-#endif
-}
 
 IOUpdate::~IOUpdate() {
     assert(mask_);
--- iod/src/bit_ops.h
+++ iod/src/bit_ops.h
@@ -3,11 +3,28 @@
 
 #include <stdint.h>
 #include <sys/types.h>
-#if VERBOSE_DEBUG
-void display(uint8_t *, size_t len = 0);
-#endif
+#include <string>
+#include <vector>
+
+std::string buffer_to_string(const uint8_t *p, size_t len);
+std::string buffer_to_string(const std::vector<uint8_t> &buf);
 
 void set_bit(uint8_t *q, unsigned int bitpos, unsigned int val);
-void copyMaskedBits(uint8_t *dest, uint8_t *src, uint8_t *mask, size_t len);
+void copyMaskedBits(uint8_t *dest, const uint8_t *src, const uint8_t *mask, size_t len);
+
+// set  a bit in the mask for each bit of this value
+void set_mask_bits(unsigned int offset, unsigned int bitpos, unsigned int bitlen, uint8_t *mask);
+
+// returns init with some bits from src, depending on the mask
+uint8_t copy_masked_bits(uint8_t init, uint8_t src, uint8_t mask);
+
+struct ChangeSet {
+    size_t count = 0;
+    std::vector<uint8_t> changes;
+};
+
+size_t copyMaskedBitsAndSetChangeMask(uint8_t *dest, uint8_t *change_mask, const uint8_t *src, const uint8_t *mask, const uint8_t *last, size_t len);
+
+ChangeSet copyMaskedBitsAndReturnMaskOfChanges(uint8_t *dest, const uint8_t *src, const uint8_t *mask, const uint8_t *last, size_t len);
 
 #endif
