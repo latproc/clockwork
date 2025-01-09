9eff3bcf7c6c20cbd1d8071d1a46303242387fbf
Martin Leadbeater <mleadbeater@me.com>
2025-01-10 06:31:07 +1030

    src/ecat_thread.cpp: Update EtherCAT timer handling to use ClockSync

--- iod/src/ecat_thread.cpp
+++ iod/src/ecat_thread.cpp
@@ -18,11 +18,18 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+#ifdef __linux__
+#define USE_CHRONO 1
+#endif
+#include "clock_sync.h"
+
 #include "ECInterface.h"
 #include "IOInterface.h"
 #include "MessageLog.h"
 #include "value.h"
-#include <bits/stdint-uintn.h>
+#include "clock.h"
+
+#include <stdint.h>
 #include <ostream>
 #include <sstream>
 #include <stdio.h>
@@ -54,25 +61,8 @@
 //#include "SetStateAction.h"
 
 //#define USE_RTC 1
-#define USE_CHRONO 1
 
-#ifdef USE_CHRONO
-#include <csignal>
-#include <ctime>
-#include <chrono>
-#elif USE_RTC
-#include <errno.h>
-#include <fcntl.h>
-#include <linux/rtc.h>
-#include <stdio.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-
-#endif
-
-#define VERBOSE_DEBUG 0
-
-extern bool machine_is_ready;
+extern bool machine_is_ready; // unsafe?
 const char *EtherCATThread::ZMQ_Addr = "inproc://ecat_thread";
 static bool machine_was_ready = false;
 uint64_t next_ecat_receive = 0;
@@ -80,7 +70,9 @@ uint64_t next_ecat_receive = 0;
 EtherCATThread::EtherCATThread()
     : status(e_collect), program_done(false), cycle_delay(1000), keep_alive(4000), last_ping(0) {}
 
-void EtherCATThread::setCycleDelay(long new_val) { cycle_delay = new_val; }
+void EtherCATThread::setCycleDelay(long new_val) {
+    cycle_delay = new_val;
+}
 
 bool EtherCATThread::waitForSync(zmq::socket_t &sync_sock) {
     char buf[10];
@@ -100,30 +92,28 @@ static bool recv(zmq::socket_t &sock, zmq::message_t &msg) {
             std::cerr << buf << "\n" << std::flush;
             return false;
         }
-        assert(false);
+        else {
+            std::stringstream ss;
+            ss << "ZMQ exception " << __FILE__ << ":" << __LINE__ << " "
+               << err.what() << " error " << zmq_strerror(errno) << "\n";
+            std::cerr << ss.str() << "\n";
+        }
     }
     return received;
 }
 
 bool EtherCATThread::checkAndUpdateCycleDelay() {
     // FIXME: potentially not thread safe
-    if (cycle_delay != get_cycle_time()) {
-        cycle_delay = get_cycle_time();
-        DBG_INITIALISATION << "setting cycle time to " << cycle_delay << "\n";
+    auto new_cycle_time = get_cycle_time();
+    if (cycle_delay != new_cycle_time) {
+        std::cerr << "setting cycle time " << cycle_delay << " -> " << new_cycle_time << "\n";
+        cycle_delay = new_cycle_time;
         ECInterface::FREQUENCY = 1000000 / cycle_delay;
         return true;
     }
     return false;
 }
 
-#if VERBOSE_DEBUG
-static void display(uint8_t *p, size_t len) {
-    for (size_t i = 0; i < len; ++i) {
-        std::cout << std::setw(2) << std::setfill('0') << std::hex << (unsigned int)p[i]
-                  << std::dec;
-    }
-}
-#endif
 
 enum DriverState { s_driver_init, s_driver_operational };
 DriverState driver_state = s_driver_init;
@@ -162,284 +152,6 @@ void setDefaultData(size_t len, uint8_t *data, uint8_t *mask) {
 #endif
 }
 
-#ifdef USE_SIGNALLER
-void sync(zmq::socket_t &clock_sync) { waitForSync(clock_sync); }
-#else
-#ifdef USE_CHRONO
-
-#include <atomic>
-#include <condition_variable>
-#include <mutex>
-#include <poll.h>
-#include <sys/syscall.h>
-
-namespace {
-
-// A pipe used by a timer thread; when a signal is recieved, a char is written
-// to the pipe. The thread can be stopped by writing a stop command to the pipe.
-int timer_pipe_fds[2] = {-1, -1};
-char monitor_stop_command = 'q';
-timer_t timerid = nullptr;
-
-std::mutex pipe_mtx;
-std::condition_variable pipe_cv; // Signalled when the pipe has been written to
-
-void handle_timer(int sig, siginfo_t *si, void *uc) {
-    if (timer_pipe_fds[1] != -1) {
-        write(timer_pipe_fds[1], ".", 1);
-    }
-    //std::cout << "Timer expired at: " << std::chrono::system_clock::now().time_since_epoch().count() << "\n";
-}
-
-void send_stop_message() {
-    write(timer_pipe_fds[1], &monitor_stop_command, 1);
-}
-
-int adjust_timer_frequency(time_t secs, unsigned long ns) {
-    struct itimerspec its;
-
-    // Set the timer period and start delay
-    its.it_value.tv_sec = secs;
-    its.it_value.tv_nsec = ns;
-    its.it_interval.tv_sec = secs;
-    its.it_interval.tv_nsec = ns;
-
-    if (timer_settime(timerid, 0, &its, nullptr) == -1) {
-        perror("timer_settime");
-        return 1;
-    }
-
-    return 0;
-}
-
-bool flush_input(int fd) {
-    char buf[100];
-    bool found_stop_signal = false;
-    while (true) {
-        struct pollfd pfd = {fd, POLLIN, 0};
-        int ret = poll(&pfd, 1, 0);
-        if (ret > 0) {
-            ssize_t num_read = read(fd, buf, sizeof(buf));
-            for (ssize_t i = 0; i < num_read; ++i) {
-                if (buf[i] == monitor_stop_command) {
-                    found_stop_signal = true;
-                }
-            }
-        } else {
-            break;  // Exit when there's no more data to read
-        }
-    }
-    if (found_stop_signal) {
-        send_stop_message();
-    }
-    return found_stop_signal;
-}
-
-void timer_thread_proc() {
-    // Configure the timer to send a signal to this thread
-    struct sigevent sev;
-    // Setup signal handler
-    struct sigaction sa;
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = handle_timer;
-    sigemptyset(&sa.sa_mask);
-    if (sigaction(SIGRTMIN, &sa, nullptr) == -1) {
-        perror("sigaction");
-        return;
-    }
-
-    sev.sigev_notify = SIGEV_THREAD_ID;
-    sev.sigev_signo = SIGRTMIN;
-    sev._sigev_un._tid = syscall(SYS_gettid);  // Get the thread ID
-    if (timer_create(CLOCK_REALTIME, &sev, &timerid) == -1) {
-        perror("timer_create");
-        return;
-    }
-
-    adjust_timer_frequency(1, 0);
-
-    char buf;
-    while (true) {
-        // Flushing the input here avoids a flurry of notifications in the case where
-        // this thread was not scheduled but signals continued writing to the pipe.
-        if (flush_input(timer_pipe_fds[0])) { break; } // abort if a stop command was found
-
-        read(timer_pipe_fds[0], &buf, 1);  // Block until the signal handler writes to the pipe
-        if (buf == monitor_stop_command) { break; }
-        std::unique_lock<std::mutex> lock(pipe_mtx);
-        pipe_cv.notify_one();
-    }
-    timer_delete(timerid);
-    timerid = nullptr;
-}
-
-
-} //namespace
-
-void sync() {
-
-    // Configure the timer rate if it has changed
-    static bool regular_timer_configured = false;
-    static long saved_frequency = ECInterface::FREQUENCY;
-    if (!regular_timer_configured || saved_frequency != ECInterface::FREQUENCY) {
-        unsigned long delay_ns = get_cycle_time() * 1000;
-        // Warning: potentially unsafe access to the timer here.
-        int config_error = adjust_timer_frequency(0, delay_ns);
-        assert("could not configure posix timer" && !config_error);
-        saved_frequency = ECInterface::FREQUENCY;
-        regular_timer_configured = true;
-    }
-
-    // Delay until the next clock tick after 50us
-    static auto last_notify = microsecs();
-    auto now = last_notify;
-    while (true) {
-        std::unique_lock<std::mutex> lock(pipe_mtx);
-        pipe_cv.wait(lock);  // Wait for the next signal
-        now = microsecs();
-        if (now - last_notify < 50) { continue; } // 
-        last_notify = now;
-        break;
-    }
-    //std::cout << "sync() returned at: " << std::chrono::system_clock::now().time_since_epoch().count() << "\n";
-}
-
-#elif USE_RTC
-int open_rtc(int rtc) {
-    if (rtc == -1) {
-        rtc = open("/dev/rtc", 0);
-        if (rtc == -1) {
-            perror("open rtc");
-            exit(1);
-        }
-    }
-    return rtc;
-}
-
-int configure_rtc_freq(int rtc) {
-    long freq = ECInterface::FREQUENCY;
-    int rc = ioctl(rtc, RTC_IRQP_SET, freq);
-    if (rc == -1) {
-        perror("set rtc freq");
-        exit(1);
-    }
-    rc = ioctl(rtc, RTC_IRQP_READ, &freq);
-    if (rc == -1) {
-        perror("ioctl");
-        exit(1);
-    }
-    std::cout << "Real time clock: freq set to : " << freq << "\n";
-
-    rc = ioctl(rtc, RTC_PIE_ON, 0);
-    if (rc == -1) {
-        perror("enable rtc pie");
-        exit(1);
-    }
-    return freq;
-}
-
-void sync() {
-    static int rtc = -1;
-    static bool rtc_timer_configured = false;
-    static long saved_frequency = ECInterface::FREQUENCY;
-    if (!rtc_timer_configured || saved_frequency != ECInterface::FREQUENCY) {
-        rtc = open_rtc(rtc);
-        saved_frequency = configure_rtc_freq(rtc);
-        assert("could not configure real time clock" && rtc >= 0);
-        rtc_timer_configured = true;
-    }
-    static uint64_t last_read_sync_check = 0;
-    {
-        unsigned long val = 0;
-        unsigned long period = 1000000 / ECInterface::FREQUENCY;
-        uint64_t t = 0;
-        while (1) {
-#ifdef KEEP_STATS
-            static Statistic clock_delay("clock");
-#endif
-            auto now = microsecs();
-            int nread = read(rtc, &val, sizeof(val));
-            t = microsecs();
-            if (nread < 0) {
-                perror("rtc-read");
-                exit(1);
-            }
-            if (last_read_sync_check) {
-                if ((t - last_read_sync_check) < 50) {
-                    continue;
-                }
-            }
-#ifdef KEEP_STATS
-            clock_delay.add(t - now);
-            if (clock_delay.getCount() >= 1000) {
-                clock_delay.report(std::cout);
-                clock_delay.reset();
-            }
-#endif
-            last_read_sync_check = t;
-            break;
-        }
-    }
-
-    // every 1000 times through here check and resync with the system cycle time
-    static int count = 0;
-    if (++count >= 1000) {
-        count = 0;
-        unsigned long freq = ECInterface::FREQUENCY;
-        unsigned long rtc_val;
-        int rc = ioctl(rtc, RTC_IRQP_READ, &rtc_val);
-        if (rc == -1) {
-            if (errno == EBADF) {
-                rtc = open("/dev/rtc", 0);
-                if (rtc == -1) {
-                    perror("open rtc");
-                    exit(1);
-                }
-            }
-            perror("read rtc");
-            exit(1);
-        }
-        else {
-            freq = rtc_val;
-        }
-        if (freq != ECInterface::FREQUENCY) {
-            std::cout << "-------------------- adjusting frequency: " << freq << "->"
-                      << ECInterface::FREQUENCY << "\n";
-            unsigned long saved_freq = freq;
-            freq = ECInterface::FREQUENCY;
-            rc = ioctl(rtc, RTC_IRQP_SET, freq);
-            if (rc == -1) {
-                perror("set rtc freq");
-                freq = saved_freq; // will retry
-            }
-            else {
-                rc = ioctl(rtc, RTC_IRQP_READ, &rtc_val);
-                if (rc != -1) {
-                    std::cout << "frequency is now " << rtc_val << "\n";
-                }
-            }
-        }
-    }
-}
-#else
-void sync() {
-    uint64_t now = nowMicrosecs();
-    int64_t delta = now - then;
-    int64_t delay = cycle_delay - delta - 100;
-    if (delay > 0) {
-        struct timespec sleep_time;
-        sleep_time.tv_sec = delay / 1000000;
-        sleep_time.tv_nsec = (delay * 1000) % 1000000000L;
-        int rc;
-        struct timespec remaining;
-        while ((rc = nanosleep(&sleep_time, &remaining) == -1)) {
-            sleep_time = remaining;
-        }
-    }
-    then = microsecs();
-}
-#endif
-#endif
 
 int EtherCATThread::sendMultiPart(zmq::socket_t *sync_sock, uint64_t global_clock) {
     // sending a four-part message, each stage may be interrupted
@@ -561,41 +273,6 @@ int EtherCATThread::sendMultiPart(zmq::socket_t *sync_sock, uint64_t global_cloc
     return stage;
 }
 
-uint64_t updateClock(uint64_t global_clock) {
-#ifdef USE_DC
-    // distributed clocks. TBD
-    static uint64_t last_ref_time = ECInterface::instance()->getReferenceTime();
-    uint32_t ref_time = ECInterface::instance()->getReferenceTime();
-    if (ref_time) {
-        uint32_t last_ref32 = last_ref_time % 0x100000000;
-        int64_t delta_ref = 0;
-        if (last_ref32 > ref_time) { // rollover
-            //std::cerr << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n";
-            delta_ref = 0x100000000 + (uint64_t)ref_time;
-            //std::cerr << std::hex << std::setw(8) << ref_time << std::dec << "\n";
-            //std::cerr << std::hex << std::setw(16) << delta_ref << std::dec << "\n";
-            //std::cerr << std::hex << std::setw(16) << last_ref32 << std::dec << "\n";
-            delta_ref -= last_ref32;
-            //std::cerr << std::hex << std::setw(16) << delta_ref << std::dec << "\n";
-        }
-        else {
-            delta_ref = ref_time - last_ref_time;
-        }
-        //int64_t err = delta_ref - period;
-        //if ( fabs(err) > (float)period/10)
-        std::cerr << "ref: " << ref_time << " cycle: " << delta_ref << " error: " << err << "\n";
-        last_ref_time += delta_ref;
-        global_clock += delta_ref;
-    }
-    else {
-        global_clock += period;
-    }
-#else
-    //global_clock += period;
-    global_clock = microsecs();
-#endif
-    return global_clock;
-}
 
 bool EtherCATThread::getEtherCatResponse(zmq::socket_t *sync_sock, uint64_t global_clock,
                                          Statistic *keep_alive_stat) {
@@ -765,16 +442,6 @@ bool EtherCATThread::getClockworkMessage(zmq::socket_t &out_sock, bool ec_ok) {
                 driver_state = s_driver_operational;
             }
         }
-#if VERBOSE_DEBUG
-        else if (cw_data && cw_mask) {
-            std::cout << "!";
-            display(cw_mask, len);
-            std::cout << "\n";
-            std::cout << "<";
-            display(cw_data, len);
-            std::cout << "\n";
-        }
-#endif
         if (ec_ok && default_data) { // only update the domain if default data has been setup
             assert("updateDomain requires cw data and mask" && cw_data && cw_mask);
             ECInterface::instance()->updateDomain(len, cw_data, cw_mask);
@@ -816,19 +483,10 @@ void EtherCATThread::operator()() {
     Statistic *keep_alive_stat = new Statistic("keep alive margin");
     Statistic::add(keep_alive_stat);
 
+
+
     unsigned long freq = ECInterface::FREQUENCY;
-#ifdef USE_CHRONO
-    if (timer_pipe_fds[0] == -1) {
-        if (pipe(timer_pipe_fds) == -1) {
-            perror("timer-pipe");
-        }
-    }
-
-    std::thread timer_thread(timer_thread_proc);
-#endif
-
-    uint64_t then = nowMicrosecs();
-    ECInterface::instance()->setReferenceTime(then % 0x100000000);
+    ClockSync clock_sync;
 
     sync_sock = new zmq::socket_t(*MessagingInterface::getContext(), ZMQ_REP);
     sync_sock->bind("inproc://ethercat_sync");
@@ -836,13 +494,6 @@ void EtherCATThread::operator()() {
     // when clockwork has output to send to EtherCAT it sends it here
     zmq::socket_t out_sock(*MessagingInterface::getContext(), ZMQ_REP);
     out_sock.bind("inproc://ethercat_output");
-
-#ifdef USE_SIGNALLER
-    zmq::socket_t clock_sync(*MessagingInterface::getContext(), ZMQ_SUB);
-    clock_sync.connect("tcp://localhost:10241");
-    int res = zmq_setsockopt(clock_sync, ZMQ_SUBSCRIBE, "", 0);
-    assert(res == 0);
-#endif
 
     uint64_t global_clock = 0;
     bool first_run = true;
@@ -852,21 +503,22 @@ void EtherCATThread::operator()() {
         usleep(100);
     }
     while (!program_done) {
-#ifdef USE_CHRONO
-        sync();
-#elif USE_RTC
-        sync();
-#elif USE_SIGNALLER
-        sync(clock_sync);
-#else
-        sync();
-#endif
         uint64_t now = nowMicrosecs();
-        ECInterface::instance()->setReferenceTime(now % 0x100000000);
         uint64_t period = 1000000 / freq;
+        {
+            auto before_sync = std::chrono::steady_clock::now();
+            clock_sync();
+            auto after_sync = std::chrono::steady_clock::now();
+            uint64_t delta = std::chrono::duration_cast<std::chrono::microseconds>(after_sync - before_sync).count();
+            auto cycle_time =  get_cycle_time();
+            if (delta > cycle_time) {
+                DBG_ETHERCAT << "Clock sync overtime: " << delta << " > " << cycle_time;
+            }
+        }
         int num_updates = 0;
 
-        // TBD the following line was missing, preventing the ec_ok logic from working
+        // machine_was ready becomes true once the machine becomes ready for the
+        // first time and stays that way.
         if (machine_is_ready && !machine_was_ready) {
             machine_was_ready = true;
         }
@@ -897,9 +549,9 @@ void EtherCATThread::operator()() {
         // exchange clockwork machine state
         next_ecat_receive = microsecs() + period / 2;
         ECInterface::instance()->receiveState();
+        global_clock = ECInterface::instance()->getApplicationTime(); //updateClock(global_clock);
 
         if (machine_is_ready && ECInterface::instance()->data.getProcessMask()) {
-            global_clock = updateClock(global_clock);
             if (status == e_collect) {
                 DBG_ETHERCAT_PACKETS << "Asking ECInterface to collect state\n";
                 num_updates = ECInterface::instance()->collectState();
@@ -944,11 +596,5 @@ void EtherCATThread::operator()() {
         ECInterface::instance()->sendUpdates();
         checkAndUpdateCycleDelay();
     }
-#ifdef USE_CHRONO
-    send_stop_message();
-    std::lock_guard<std::mutex> lock(pipe_mtx);
-    pipe_cv.notify_one();  // Wake up sync if it is waiting
-    timer_thread.join();
-#endif
     keep_alive_stat->report(std::cout);
 }
