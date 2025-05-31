0fb402bbf78d4c83250d6f43f3d5e444b8b8d9ca
Martin Leadbeater <mleadbeater@me.com>
2025-05-31 12:32:39 +0930

    plugins:build.sh: allow for more flexible build options on different platforms

--- plugins/build.sh
+++ plugins/build.sh
@@ -13,23 +13,29 @@ out=`cat $script | awk '
 	$0 ~ /.*PLUGIN[^"]*"[a-zA-Z0-9_.]+".*;/ {
 	sub(/.*PLUGIN[^"]*"/,"",$0)
 	sub(/".*/,"",$0)
-	print 
+	print
 	}'`
 
 cat "$script" | awk -v file="$script" '
 	BEGIN {
 #		print "#include \"plugin.inc\""
-	} 
-	/^%END_PLUGIN/ {copy=0} 
-	copy==1 
-	$1 ~ /^%BEGIN_PLUGIN/ { 
+	}
+	/^%END_PLUGIN/ {copy=0}
+	copy==1
+	$1 ~ /^%BEGIN_PLUGIN/ {
 		printf "#line %d \"%s\"\n", NR+1, file
 		copy=1
-	}  
+	}
 	' >plugin_$$.c
 
-[ `uname -s` == "Linux" ] && LDFLAGS="$LDFLAGS -shared -Wall -fPIC -Wl,-soname,$out,-undefined,dynamic_lookup"
-[ `uname -s` == "Darwin" ] && LDFLAGS="$LDFLAGS -dynamiclib -Wall -pedantic -fPIC -Wl,-undefined,dynamic_lookup"
-
-echo gcc $CFLAGS $LDFLAGS -I /usr/local/include -I../iod/src plugin_$$.c -o "$out" 
-gcc $CFLAGS $LDFLAGS -I../iod/src plugin_$$.c -o "$out" && rm plugin_$$.c
+if [ $(uname -s) == "Darwin" ]; then
+    LDFLAGS="$LDFLAGS -dynamiclib -Wall -pedantic -fPIC -Wl,-undefined,dynamic_lookup"
+    echo gcc $CFLAGS $LDFLAGS -I../iod/src plugin_$$.c -o "$out"
+    gcc $CFLAGS $LDFLAGS -I../iod/src plugin_$$.c -o "$out" && rm plugin_$$.c
+elif [ $(uname -s) == "Linux" ]; then
+    LDFLAGS="$LDFLAGS -shared -Wall -fPIC -Wl,-soname,$out"
+    echo gcc $CFLAGS -rdynamic $LDFLAGS -I../iod/src plugin_$$.c -o "$out"
+    gcc $CFLAGS $LDFLAGS -I../iod/src plugin_$$.c -o "$out" && rm plugin_$$.c
+else
+    echo "unknown platform $(uname -s)"
+fi
