8a8ffe40c49f916716b54fb60a188276fe11481f
Martin Leadbeater <mleadbeater@me.com>
2025-09-25 00:32:01 +0930

    Add web request plugin

---
+++ iod/src/exec_web_request.h
@@ -0,0 +1,16 @@
+#ifndef __EXEC_WEB_REQUEST_H__
+#define __EXEC_WEB_REQUEST_H__
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int exec_web_request(void *scope);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __EXEC_WEB_REQUEST_H__
