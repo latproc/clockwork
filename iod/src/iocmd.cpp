0b1be7ca8d8b7df6dc1c80e3961559dc744d7a33
Martin Leadbeater <mleadbeater@me.com>
2025-11-09 21:20:02 +1030

    iocmd: Add JSON support
    Also add a commandline parameter: --eol=<char> to set the line terminator
    The parameter can be put into the input stream/file: eol=;

--- iod/src/iocmd.cpp
+++ iod/src/iocmd.cpp
@@ -19,21 +19,34 @@
 */
 
 #include "Logger.h"
-#include <inttypes.h>
 #include <iostream>
-#include <iterator>
 #include <sstream>
 #include <stdio.h>
 #include <string>
 #include <sys/types.h>
 #include <zmq.hpp>
+#include "MessageEncoding.h"
+#include "value.h"
+#include <list>
 #define _MAIN_
 
-zmq::context_t *context;
+zmq::context_t *context = nullptr;
+zmq::socket_t *psocket = nullptr;
+std::string line_input;
 
-void sendMessage(zmq::socket_t &socket, const char *message) {
+std::list<Value> params;
+bool quiet = true;
+char eol_char = '\n'; // Default end-of-line character
+bool cmdline_done = false;
+
+extern void yyparse();
+extern FILE *yyin;
+
+bool sendMessage(zmq::socket_t &socket, const char *message) {
     const char *msg = (message) ? message : "";
     size_t len = strlen(msg);
+    if (len == 0) { return false; }
+    std::cout << "Sending: " << msg << "\n";
     zmq::message_t reply(len);
     memcpy((void *)reply.data(), msg, len);
     while (true) {
@@ -48,14 +61,108 @@ void sendMessage(zmq::socket_t &socket, const char *message) {
             throw;
         }
     }
+    return true;
 }
 
-zmq::socket_t *psocket = 0;
+char *send_command(std::list<Value> &params) {
+    if (params.size() == 0) {
+        return 0;
+    }
+    Value cmd_val = params.front();
+    params.pop_front();
+    std::string cmd = cmd_val.asString();
+    std::string msg = MessageEncoding::encodeCommand(cmd, params);
+    sendMessage(*psocket, msg.c_str());
+    size_t size;
+    zmq::message_t reply;
+    if (psocket->recv(&reply)) {
+        size = reply.size();
+        char *data = (char *)malloc(size + 1);
+        if (data) {
+            memcpy(data, reply.data(), size);
+            data[size] = 0;
+        }
+        return data;
+    }
+    return nullptr;
+}
+void process_command(std::list<Value> &params) {
+    char *data = send_command(params);
+    if (data) {
+        std::cout << data << "\n";
+        free(data);
+    }
+}
 
-extern void yyparse();
-extern FILE *yyin;
+std::string trim(const std::string& str) {
+    size_t first = str.find_first_not_of(" \t\r\n");
+    if (first == std::string::npos) {
+        return ""; // String is all whitespace
+    }
+    size_t last = str.find_last_not_of(" \t\r\n");
+    return str.substr(first, (last - first + 1));
+}
 
-void usage(const char *name) { std::cout << name << " [-q] [-h host] [-p port]\n"; }
+// Read a line from input stream until the specified delimiter character is found
+bool getLineWithDelimiter(std::istream& input, std::string& cmd, char delimiter) {
+    cmd.clear();
+    int ch;
+    while (true) {
+        std::string cmd;
+        while ((ch = input.get()) != EOF) {
+            if (ch == delimiter) {
+                break;
+            }
+            if (ch == '\r' || ch == '\n' || ch == '\t') {
+                ch = ' ';
+            }
+            cmd += static_cast<char>(ch);
+        }
+        if (ch == EOF) {
+            return false;
+        }
+        if (cmd.compare(0, 4, "eol=") == 0 && cmd.length() >= 5) {
+            delimiter = cmd[4];
+            if (!quiet) {
+                std::cerr << "EOL character changed to: ";
+                if (isprint(delimiter)) {
+                    std::cerr << "'" << delimiter << "'";
+                }
+                else {
+                    std::cerr << std::oct << delimiter << std::dec << " (" << (int)delimiter << ")";
+                }
+                std::cerr << "\n";
+            }
+            continue;
+        }
+        if (cmd.empty()) { continue; }
+        if (!quiet) {
+            std::cout << cmd << "\n";
+        }
+
+        line_input = trim(cmd);
+        return true;
+    }
+}
+
+std::string get_input_line() {
+    std::string line;
+    if (!getLineWithDelimiter(std::cin, line, eol_char)) {
+        cmdline_done = true;
+        return "";
+    }
+    line_input += ";";
+    return line_input;
+}
+
+void usage(const char *name) { 
+    std::cout << name << " [-v] [-h host] [-p port] [--eol=<char>]\n"
+              << "  -v         : verbose mode\n"
+              << "  -h host    : host to connect to (default: 127.0.0.1)\n"
+              << "  -p port    : port to connect to (default: 5555)\n"
+              << "  --eol=<c>  : end-of-line character (default: newline)\n"
+              << "               Use 'eol=<c>' in stdin to change dynamically\n";
+}
 
 const char *program_name;
 
@@ -68,8 +175,8 @@ int main(int argc, const char *argv[]) {
 
     try {
         int port = 5555;
-        bool quiet = false;
         std::string host = "127.0.0.1";
+
         for (int i = 1; i < argc; ++i) {
             if (i < argc - 1 && strcmp(argv[i], "-p") == 0) {
                 port = (int)strtol(argv[++i], 0, 10);
@@ -77,37 +184,29 @@ int main(int argc, const char *argv[]) {
             else if (i < argc - 1 && strcmp(argv[i], "-h") == 0) {
                 host = argv[++i];
             }
+            else if (strncmp(argv[i], "--eol=", 6) == 0) {
+                const char *eol_str = argv[i] + 6;
+                if (strlen(eol_str) > 0) {
+                    eol_char = eol_str[0];
+                }
+            }
             else if (strcmp(argv[i], "-?") == 0) {
                 usage(argv[0]);
                 exit(0);
             }
-            else if (strcmp(argv[i], "-q") == 0) {
-                quiet = true;
+            else if (strcmp(argv[i], "-v") == 0) {
+                quiet = false;
             }
         }
         std::stringstream ss;
         ss << "tcp://" << host << ":" << port;
         std::string url(ss.str());
-        zmq::socket_t socket(*context, ZMQ_REQ);
+        psocket = new zmq::socket_t(*context, ZMQ_REQ);
         int linger = 0; // do not wait at socket close time
-        socket.setsockopt(ZMQ_LINGER, &linger, sizeof(linger));
+        psocket->setsockopt(ZMQ_LINGER, &linger, sizeof(linger));
 
-        socket.connect(url.c_str());
-        std::string cmd;
-        while (std::getline(std::cin, cmd)) {
-            sendMessage(socket, cmd.c_str());
-            if (!quiet) {
-                std::cout << cmd << "\n";
-            }
-            zmq::message_t reply;
-            if (socket.recv(&reply)) {
-                auto size = reply.size();
-                char *data = (char *)malloc(size + 1);
-                memcpy(data, reply.data(), size);
-                data[size] = 0;
-                std::cout << data << "\n";
-            }
-        }
+        psocket->connect(url.c_str());
+        yyparse();
     }
     catch (const std::exception &e) {
         if (zmq_errno()) {
