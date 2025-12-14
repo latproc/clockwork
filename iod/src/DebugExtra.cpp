91ebb1591f6c99190aec4f090f2d3cfb3fc51f0f
Martin Leadbeater <mleadbeater@me.com>
2025-12-14 11:30:49 +1030

    Restructure logging setup

--- iod/src/DebugExtra.cpp
+++ iod/src/DebugExtra.cpp
@@ -23,23 +23,31 @@
 
 DebugExtra *DebugExtra::instance_ = 0;
 DebugExtra::DebugExtra() {
-    DEBUG_PARSER = LogState::instance()->define("DEBUG_PARSER");
-    DEBUG_PREDICATES = LogState::instance()->define("DEBUG_PREDICATES");
-    DEBUG_MESSAGING = LogState::instance()->define("DEBUG_MESSAGING");
-    DEBUG_STATECHANGES = LogState::instance()->define("DEBUG_STATECHANGES");
-    DEBUG_SCHEDULER = LogState::instance()->define("DEBUG_SCHEDULER");
-    DEBUG_AUTOSTATES = LogState::instance()->define("DEBUG_AUTOSTATES");
-    DEBUG_MACHINELOOKUPS = LogState::instance()->define("DEBUG_MACHINELOOKUPS");
-    DEBUG_PROPERTIES = LogState::instance()->define("DEBUG_PROPERTIES");
-    DEBUG_DEPENDANCIES = LogState::instance()->define("DEBUG_DEPENDANCIES");
-    DEBUG_ACTIONS = LogState::instance()->define("DEBUG_ACTIONS");
-    DEBUG_INITIALISATION = LogState::instance()->define("DEBUG_INITIALISATION");
-    DEBUG_MODBUS = LogState::instance()->define("DEBUG_MODBUS");
-    DEBUG_DISPATCHER = LogState::instance()->define("DEBUG_DISPATCHER");
-    DEBUG_CHANNELS = LogState::instance()->define("DEBUG_CHANNELS");
-    DEBUG_ETHERCAT = LogState::instance()->define("DEBUG_ETHERCAT");
-    DEBUG_PROCESSING = LogState::instance()->define("DEBUG_PROCESSING");
-    DEBUG_ETHERCAT_CALLS = LogState::instance()->define("DEBUG_ETHERCAT_CALLS");
-    DEBUG_ETHERCAT_SDO = LogState::instance()->define("DEBUG_ETHERCAT_SDO");
-    DEBUG_ETHERCAT_PACKETS = LogState::instance()->define("DEBUG_ETHERCAT_PACKETS");
+    auto logstate = LogState::instance();
+    DEBUG_PARSER = logstate->define("DEBUG_PARSER");
+    DEBUG_PREDICATES = logstate->define("DEBUG_PREDICATES");
+    DEBUG_MESSAGING = logstate->define("DEBUG_MESSAGING");
+    DEBUG_STATECHANGES = logstate->define("DEBUG_STATECHANGES");
+    DEBUG_SCHEDULER = logstate->define("DEBUG_SCHEDULER");
+    DEBUG_AUTOSTATES = logstate->define("DEBUG_AUTOSTATES");
+    DEBUG_MACHINELOOKUPS = logstate->define("DEBUG_MACHINELOOKUPS");
+    DEBUG_PROPERTIES = logstate->define("DEBUG_PROPERTIES");
+    DEBUG_DEPENDANCIES = logstate->define("DEBUG_DEPENDANCIES");
+    DEBUG_ACTIONS = logstate->define("DEBUG_ACTIONS");
+    DEBUG_INITIALISATION = logstate->define("DEBUG_INITIALISATION");
+    DEBUG_MODBUS = logstate->define("DEBUG_MODBUS");
+    DEBUG_DISPATCHER = logstate->define("DEBUG_DISPATCHER");
+    DEBUG_CHANNELS = logstate->define("DEBUG_CHANNELS");
+    DEBUG_ETHERCAT = logstate->define("DEBUG_ETHERCAT");
+    DEBUG_PROCESSING = logstate->define("DEBUG_PROCESSING");
+    DEBUG_ETHERCAT_CALLS = logstate->define("DEBUG_ETHERCAT_CALLS");
+    DEBUG_ETHERCAT_SDO = logstate->define("DEBUG_ETHERCAT_SDO");
+    DEBUG_ETHERCAT_PACKETS = logstate->define("DEBUG_ETHERCAT_PACKETS");
+}
+
+DebugExtra *DebugExtra::instance() {
+    if (!instance_) {
+        instance_ = new DebugExtra;
+    }
+    return instance_;
 }
