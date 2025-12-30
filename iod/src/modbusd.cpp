be756d188436049cacba902512811c10a03a4ef6
Martin Leadbeater <mleadbeater@me.com>
2025-12-30 13:44:11 +1030

    modbusd.cpp: Fix leak and incorrect double send to IOD

--- iod/src/modbusd.cpp
+++ iod/src/modbusd.cpp
@@ -697,8 +697,6 @@ char *sendIODMessage(const std::string &s);
 std::string getIODSyncCommand(int group, int addr, bool which) {
     int new_value = (which) ? 1 : 0;
     auto msg = MessageEncoding::encodeCommand("MODBUS", Value{group}, Value{addr}, Value{new_value});
-    //sendIODMessage(msg);
-
     if (DEBUG_BASIC) {
         std::cout << "IOD command: " << msg << "\n";
     }
@@ -707,8 +705,6 @@ std::string getIODSyncCommand(int group, int addr, bool which) {
 
 std::string getIODSyncCommand(int group, int addr, int new_value) {
     auto msg = MessageEncoding::encodeCommand("MODBUS", Value{group}, Value{addr}, Value{new_value});
-    sendIODMessage(msg);
-
     if (DEBUG_BASIC) {
         std::cout << "IOD command: " << msg << "\n";
     }
@@ -717,8 +713,6 @@ std::string getIODSyncCommand(int group, int addr, int new_value) {
 
 std::string getIODSyncCommand(int group, int addr, unsigned int new_value) {
     auto msg = MessageEncoding::encodeCommand("MODBUS", Value{group}, Value{addr}, Value{new_value});
-    sendIODMessage(msg);
-
     if (DEBUG_BASIC) {
         std::cout << "IOD command: " << msg << "\n";
     }
